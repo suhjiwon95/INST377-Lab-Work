d8cfc03cbb9351bc1107d90aad96fb2a5028d4d2
Jiwon Suh <suhjiwon1995@gmail.com>
2023-02-07 14:17:43 -0500

    updated js

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -27,11 +27,13 @@ function updateSlidePosition() {
     slide.classList.remove('visible');
     slide.classList.add('hidden');
   });
-  
-  slides[slidePosition].classList.add('visible');
 
   // outside your .forEach,
   // add a 'visible' class to the slide at the current slidePosition in slides
+  console.log(slidePosition);
+  slides[slidePosition].classList.add('visible');
+
+
 }
 
 function moveToNextSlide() {
@@ -41,6 +43,11 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
+ if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+ } else {
+    slidePosition += 1;
+ }
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
@@ -51,6 +58,11 @@ function moveToPrevSlide() {
     and if so, sets your slidePosition to the last slide position in totalSlides
     if not, set the slidePosition to the current position minus one
   */
+    if (slidePosition === 0) {
+      slidePosition = totalSlides - 1;
+   } else {
+      slidePosition -= 1 ;
+   }
   updateSlidePosition();
 }
 
