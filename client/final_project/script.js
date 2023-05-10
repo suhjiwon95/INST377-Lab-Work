c6e909b013a4046e6c4f61695fad489c3fe3b769
Jiwon Suh <suhjiwon1995@gmail.com>
2023-05-10 18:21:33 -0400

    last commit for final project

--- client/final_project/script.js
+++ client/final_project/script.js
@@ -1,43 +1,110 @@
-function getTime() {
-    const address = document.getElementById('address').value;
-    const url = `https://api.sunrise-sunset.org/json?formatted=0&date=today&address=${address}`;
+async function mainEvent() {
+    const submitButton = document.getElementById('submit');
+    const slider = document.getElementById("slider");
+    const existingText = document.getElementById("time");
+    const showSlider = document.querySelector(".hidden");
 
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        const sunsetTime = new Date(data.results.sunset);
-        const sunsetTimeString = sunsetTime.toLocaleTimeString();
-        const sunriseTime = new Date(data.results.sunrise);
-        const sunriseTimeString = sunriseTime.toLocaleTimeString();
-        document.getElementById('sunset-time').textContent = `${sunsetTimeString}`;
-        document.getElementById('sunset-time').textContent = `${sunriseTimeString}`;
-        console.log('sunset', sunsetTimeString);
-        console.log('sunrise', sunriseTimeString);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-      });
+    // Use Geolocation API to get the current location's latitude and longitude
+    submitButton.addEventListener("click", () => {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const long = position.coords.longitude;
+            console.log(lat);
+            console.log(long);
+            const sunriseSunsetUrl = `https://api.sunrise-sunset.org/json?formatted=0&lat=${lat}&lng=${long}&date=today`;
+            console.log("fetching sunrise sunset API")
+            
+            fetch(sunriseSunsetUrl)
+                .then(response => response.json())
+                .then(data => {
+                    const { sunrise, sunset } = data.results;
+                    console.log(`Sunrise: ${sunrise}, Sunset: ${sunset}`);
+                    const sunsetTime = new Date(data.results.sunset).toLocaleTimeString(); 
+                    const sunriseTime = new Date(data.results.sunrise).toLocaleTimeString(); 
+                    hideElement();
+                    replaceElement();
+                    const currentTime = document.getElementById('time').textContent = `${sunsetTime}`;
+                    
+                    // enable the hidden slider
+                    showSlider.style.display = 'block';
+                    
+                    slider.addEventListener("input", event => {
+                        const value = event.target.value;
+                        // when users move the slider to the left end, displats sunrise time
+                        if (value == slider.min) {
+                            replaceSunset();
+                            text = document.getElementById('time').textContent = `${sunriseTime}`;
+                            existingText.html(text);
+                        }
+                        // when users move the slider to the right end, displays sunset time
+                        if (value == slider.max) {
+                            replaceElement();
+                            text = document.getElementById('time').textContent = `${sunsetTime}`;
+                            existingText.html(text);
+                        }
+                      });
+                    
+                })
+                .catch(error => console.error(error));
+            
+           
+        });
+
+    })
     
-    
-    hideElement();
-    replaceElement();
+
+  
+}
+function hideElement() {
+    const hideSearchBar = document.getElementById('submit');
+    hideSearchBar.style.display = 'none';
     
 }
-
-function hideElement() {
-    const hideSearchBar = document.getElementById('search_bar');
-    hideSearchBar.style.display = 'none';
-    console.log('hide');
-  }
   
 
 function replaceElement() {
     const title = document.querySelector('h1');
-    const replaceTitle = 'Sunset (UTC)';
+    const replaceTitle = 'Sunset';
     title.textContent = replaceTitle;
-    console.log('replace');
-    return false;
-  }
-  
+}
 
-//document.addEventListener('DOMContentLoaded', async () => getSunsetTime()); // the async keyword means we can make API requests
+function replaceSunset() {
+const title = document.querySelector('h1');
+const replaceTitle = 'Sunrise';
+title.textContent = replaceTitle;
+}
+
+
+
+ 
+// function getTime() {
+//     const address = document.getElementById('address').value;
+//     const url = `https://api.sunrise-sunset.org/json?formatted=0&date=today&address=${address}`;
+
+//     fetch(url)
+//       .then(response => response.json())
+//       .then(data => {
+//         const sunsetTime = new Date(data.results.sunset);
+//         const sunsetTimeString = sunsetTime.toLocaleTimeString();
+//         const sunriseTime = new Date(data.results.sunrise);
+//         const sunriseTimeString = sunriseTime.toLocaleTimeString();
+//         document.getElementById('sunset-time').textContent = `${sunsetTimeString}`;
+//         document.getElementById('sunset-time').textContent = `${sunriseTimeString}`;
+//         console.log('sunset', sunsetTimeString);
+//         console.log('sunrise', sunriseTimeString);
+//       })
+//       .catch(error => {
+//         console.error('Error:', error);
+//       });
+    
+    
+//     hideElement();
+//     replaceElement();
+ 
+// }
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
--- client/final_project/sketch.js
+++ client/final_project/sketch.js
@@ -1,49 +1,34 @@
-// for red, green, and blue color values
-let r, g, b;
-let sliderValue = 0;
+let colorValue = '#FDAE46';
+let colorSlider;
+
 
 function setup() {
     createCanvas(2000, 1000);
-    // slider has a range between 0 and 255 with a starting value of 127
-    slider = createSlider(0, 255, 0);
-    slider.position(width/2, height - height/4);
-    slider.style('width', '540px');
-    // Attach a callback function to the 'input' event of the slider
-    slider.input(() => {
-        // Update the value of sliderValue
-        sliderValue = slider.value();
-    });
+    colorSlider = select("#slider");
+    colorSlider.input(updateColorValue);
 
-    // Pick colors randomly
-    r = random(255);
-    g = random(255);
-    b = random(255);
-
-    
 }
 
 function draw() {
-    // Set the hue according to the slider
-    fill(slider.value(),r, g, b);
     noStroke();
-    // fill('#FDAE46');
+    fill(colorValue,0,0);
     circle(width/3.4,height/2,80);
     circle(width/3.4,height/3,80);
     circle(width/3,height/6,80);
     circle(width/2.4,height/16,80);
-    
-    // Display the current value of the slider
-    textSize(20);
-    text("Slider Value: " + sliderValue, 10, 70);
 
-    // If the slider is at its maximum value
-    if (sliderValue == slider.elt.max) {
-        // Display a message on the web page
-        textSize(30);
-        text("Slider is at the end!", 10, 150);
-    }
-    
-     
-   
+
+
 }
+
+function updateColorValue() {
+    colorValue = colorSlider.value();
+    if (colorValue == colorSlider.elt.max) {
+        background('#181200');
+    }
+    if (colorValue == colorSlider.elt.min) {
+        background('#FFFAF2');
+    }
+}
+
 
